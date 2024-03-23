20ac97506cd24a7fc82e5471e0630e00a652be0c
BorakL <lborak88@gmail.com>
2024-03-23 02:34:23 +0100

    learning puppeteer

---
+++ learning/findElements.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+
+async function checkElementsPresent(url,elements){
+    try{
+        const browser = await puppeteer.launch({headless:false});
+        const page = await browser.newPage();
+        await page.goto(url);
+        const presenceResults = [];
+        for(const element of elements){
+            const foundElements = await page.$$(element);
+            presenceResults[element] = foundElements.length>0
+        }
+        console.log("Elements Present");
+        console.log(presenceResults)
+    }catch(error){
+        console.log("Unable to check elements in the URL: ",error)
+    }
+}
+
+const elements = ["div", "#main-content", 'footer']
+
+checkElementsPresent('http://example.com', elements)
